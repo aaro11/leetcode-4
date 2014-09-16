ff9d498d4fb9f28b6b357c4bbcc41a35d545b661
wanqu2014yangyuan <326098173@qq.com>
2014-09-16 14:36:17 +0800

    no message

--- Max_Points_on_a_Line/main.cpp
+++ Max_Points_on_a_Line/main.cpp
@@ -1,5 +1,6 @@
 
 #include <vector>
+#include <map>
 using namespace std;
 
 //Definition for a point.
@@ -10,81 +11,90 @@ struct Point {
     Point(int a, int b) : x(a), y(b) {}
 };
 
-class Solution 
+//map<int, string> mapStudent;
+//mapStudent.insert(pair<int, string>(1, "student_one "));
+//mapStudent.insert(pair<int, string>(2, "student_two "));
+//mapStudent.insert(pair<int, string>(3, "student_three "));
+//map<int, string>::iterator iter;
+//iter = mapStudent.find(1);
+//if (iter != mapStudent.end())
+//{
+//	cout << "Find, the value is: " << iter->second << endl;
+//}
+//else
+//{
+//	cout << "Do not Find" << endl;
+//}
+//return 0;
+
+
+
+class Solution
 {
 private:
-	int m_k;
-	int m_b;
-
-	inline void set_k_b(Point p1, Point p2)
-	{
-		if ((p1.x - p2.x) == 0)
-		{
-			m_k = 0;
-		}
-		else
-		{
-			m_k = (p1.y - p2.y) / (p1.x - p2.x);
-		}
-		m_b = p1.y - m_k*p1.x;				// b = y2 - k*x2;
-	}
-
-	inline bool isInLine(Point p)
-	{
-		return p.y == m_k * p.x + m_b;
-	}
-
-	int getCountInLine(vector<Point> points)
-	{
-		int count = 0;
-		int len = points.size();
-		for (int i = 0; i<len; i++)
-		{
-			if (isInLine(points[i]))
-			{
-				count++;
-			}
-		}
-		return count;
-	}
-
 
 
 public:
 	int maxPoints(vector<Point> &points)
 	{
-		int max = 0;
+		map<double, int> slopes;
+		int count = 0;
+
 		int len = points.size();
 		if (len == 1)
 			return 1;
 		for (int i = 0; i<len; i++)
 		{
+			int max = 0;
+			slopes.clear();
+			map<double, int>::iterator iter;
+			int sameCount = 0;
 			for (int j = i + 1; j<len; j++)
 			{
-				// 在通过点求直线 k b
-				set_k_b(points[i], points[j]);
-
-				// 然后遍历其他点在直线上的数量
-				int count = getCountInLine(points);
-				if (max < count)
+				if ((points[i].x == points[j].x) && (points[i].y == points[j].y))
 				{
-					max = count;
+					sameCount++;
+					continue;
+				}
+					
+				//求其他点与这条直线的斜率
+				double slope = (points[i].x - points[j].x == 0) ? 0 : (points[i].y - points[j].y + 0.0) / (points[i].x - points[j].x);
+				iter = slopes.find(slope);
+				if (iter == slopes.end())
+				{
+					slopes[slope] = 2;
+				}
+				else
+				{
+					slopes[slope]++;
 				}
 			}
+			//遍历map求里面值最大的
+			for (iter = slopes.begin(); iter != slopes.end(); iter++)
+			{
+				if (max < (iter->second + sameCount))
+				{
+					max = iter->second + sameCount;
+				}
+			}
+			
+			if (max < sameCount + 1)
+				max = sameCount + 1;
+
+			if (count < max)
+			{
+				count = max;
+			}
 		}
-		return max;
+		return count;
 	}
 };
 
 int main()
 {
 	vector<Point> points;
-	points.push_back(Point(29, 87));
-	points.push_back(Point(145, 227));
-	points.push_back(Point(400, 84));
-	points.push_back(Point(800, 179));
-	points.push_back(Point(60, 950));
-	points.push_back(Point(560, 122));
+	points.push_back(Point(0, 0));
+	points.push_back(Point(0, 0));
 
 	Solution s;
 	int a = s.maxPoints(points);
