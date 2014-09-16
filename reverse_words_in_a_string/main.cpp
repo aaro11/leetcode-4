d48139144fc1eac708c4d55d255b43458eb5335a
wanqu2014yangyuan <326098173@qq.com>
2014-09-16 15:06:41 +0800

    字符串翻转

--- reverse_words_in_a_string/main.cpp
+++ reverse_words_in_a_string/main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 using namespace std;
 
+//http://baike.baidu.com/view/1005388.htm?fr=aladdin#4
+
 //string &operator=(const string &s);//把字符串s赋给当前字符串
 //string &assign(const char *s);//用c类型字符串s赋值
 //string &assign(const char *s, int n);//用c字符串s开始的n个字符赋值
@@ -11,24 +13,41 @@ using namespace std;
 //string &assign(const string &s, int start, int n);//把字符串s中从start开始的n个字符赋给当前字符
 //string &assign(const_iterator first, const_itertor last); //把first和last迭代器之间的部分赋给字符串
 
-
+//两次翻转 单词翻转 句子翻转
 class Solution {
+
+private:
+	void reverseStr(string& s, string::size_type left, string::size_type right)
+	{
+		while (left < right)
+		{
+			swap(s[left++], s[right--]);
+		}
+	}
 public:
 	void reverseWords(string &s) {
 		if (s.empty())
-			return;
-		// 妈的我不想用string 
-		char *p = s.c_str();
+		{
+			return ;
+		}
+		string ret = s;
+		string::size_type left, right;
+		left = s.find_first_not_of(' ', 0);
+		right = s.find_first_of(' ', left);
+		while (right != string::npos)
+		{
+			reverseStr(ret, left, right - 1);
+			left = s.find_first_not_of(' ', right);
+			right = s.find_first_of(' ', left);
+		}
+		if (left != string::npos)
+		{
+			reverseStr(ret, left, ret.size() - 1);
+		}
+		reverseStr(ret, 0, ret.size() - 1);
 
-		vector<string> words;
-
-		string tmp = s;
-
-		// 查找" "的第一个位置
-		int pos = s.find_first_of(" ");
-
-		words.push_back( s.assign(s.c_str(), pos));
-		
+		s = ret;
+		return ;
 	}
 };
 
