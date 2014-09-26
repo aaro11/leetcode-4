4edb61ab1908f6aacadbdee10b1cca6b0b353a59
wanqu2014yangyuan <326098173@qq.com>
2014-09-26 16:11:22 +0800

    lua table深拷贝

---
+++ table_copy.lua
@@ -0,0 +1,11 @@
+function copyTab(st)
+    local tab = {}
+    for k, v in pairs(st or {}) do
+        if type(v) ~= "table" then
+            tab[k] = v
+        else
+            tab[k] = copyTab(v)
+        end
+    end
+    return tab
+end
