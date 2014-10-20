2060bac3da7f4a4718b6d2e41c4c2dd3481d2d1e
wanqu2014yangyuan <326098173@qq.com>
2014-10-20 11:42:11 +0800

    no message

--- 《数据结构及算法经典》源代码/数据结构及算法经典/单元加/erre.c
+++ 《数据结构及算法经典》源代码/数据结构及算法经典/单元加/erre.c
@@ -1,25 +1,34 @@
-#include "stdio.h"
-void num(int number,int i)
+
+#include <stdio.h>
+
+void num(int number, int i)
 {
-int k,n=number;
-while(n>i)
-{
-  n=n-i;
-  if((n+i)!=number)
-    {
-      for(k=0;number-k>n;k=k+i)
-          printf("%d+",i);
-      printf("%d\n",n);
-  }
-  else
-    printf("%d+%d\n",i,n);
+	int k, n = number;
+	while (n > i)
+	{
+		n = n - i;
+		if ((n + i) != number)
+		{
+			for (k = 0; number - k > n; k = k + i)
+			{
+				printf("%d+", i);
+			}				
+			printf("%d\n", n);
+		}
+		else
+		{
+			printf("%d+%d\n", i, n);
+		}			
+	}
 }
-}
-void main()
+int main()
 {
-int j,m;
-printf("input:");
-scanf("%d",&m);
-for(j=1;j<=m/2;j++)
-    num(m,j);
+	int j, m;
+	printf("input:");
+	scanf_s("%d", &m);
+	for (j = 1; j <= m / 2; j++)
+	{
+		num(m, j);
+	}
+	return 0;
 }
